7a7f6bd364e7e0318b1130fab994653c127a5352
Joey3020 <joonho0320@gmail.com>
2020-04-11 16:42:16 +0900

    modified data processing

--- read_and_process_results_3.m
+++ read_and_process_results_3.m
@@ -5,11 +5,13 @@
 % compare hpbw, steared angle, maximum directivity vs original beam
 
 predictions = xlsread('predictions', 1);
-Directivity = xlsread('testdata', 1);
+labels = xlsread('testdata', 2);
 
 num = 64;
 lamda = 1;
 k = 2 * pi() / lamda;
+n0 = 120 * pi();
+
 testdatasize = 100;
 
 color_label_green = 1;
@@ -27,14 +29,22 @@ for iter = 1:1:testdatasize
     y0 = predictions(iter, 4);
     d = predictions(iter, 5);
     
+    
+        
+    a_label = labels(iter,1);
+    b_label = labels(iter,2);
+    mode_label = labels(iter,3);
+    phase_label = labels(iter,4);
+    
+    
     % get aperture field from predicted soure pos
     % and get directivity matrix 
     [Exa, Eya, Hxa, Hya] = Aperture_field_from_point_source(x0, y0, d, a, b, lamda, num);
     D_r = Get_Directivity_General(Exa, Eya, Hxa, Hya, a, b, lamda, num);
     
     % test beam directivity matrix
-    D = Directivity(iter, :);
-    D = reshape(D, num, num);
+    [Ex_label, Ey_label] = Set_E_field(mode_label, phase_label, a_label, b_label, num);
+    D = Get_Directivity_General(Ex_label, Ey_label, -Ey_label/n0, Ex_label/n0, a_label, b_label, lamda, num);
     
     
     %save results
