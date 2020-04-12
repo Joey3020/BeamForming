09fc8bdc2d5ca729081829b98b71b11e92af2e81
Joey3020 <joonho0320@gmail.com>
2020-04-13 01:51:07 +0900

    add data saver by class

--- save_train_single_source_by_classes.m
+++ save_train_single_source_by_classes.m
@@ -1,4 +1,14 @@
+data_size = 250;
+lamda = 1;
+num = 64;
+
+for k = 1:250
 
 
-datasize = 1;
-[data, label] = DL_generate_data_by_class(datasize, 1, 64);
+     [ data, labels] = DL_generate_data_by_class(data_size, lamda, num);
+
+
+    filename = sprintf('data_with_source_by_class%d.xlsx', k);
+    writematrix(data, filename, 'Sheet', 1);
+    writematrix(labels, filename, 'Sheet', 2);
+end
