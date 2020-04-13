26f8030e24a2b0a1c685641a265d332268de1e26
Joey3020 <joonho0320@gmail.com>
2020-04-14 01:10:22 +0900

    modified read and process to accomodate data for fixed X

--- read_and_process_results_3.m
+++ read_and_process_results_3.m
@@ -4,15 +4,16 @@
 % recover beam from prediction results
 % compare hpbw, steared angle, maximum directivity vs original beam
 
-predictions = xlsread('C:\Users\User\Documents\Beamforming\predictions (3).xlsx', 1);
-labels = xlsread('C:\Users\User\Documents\Beamforming\testdata_without_source.xlsx', 2);
+predictions = xlsread('C:\Users\joonho0320\Documents\result\predictions.xlsx', 1);
+labels = xlsread('C:\Users\joonho0320\Documents\result\testdata_without_source1.xlsx', 2);
 
 num = 64;
 lamda = 1;
 k = 2 * pi() / lamda;
 n0 = 120 * pi();
 
-testdatasize = 20;
+
+testdatasize = 200;
 
 color_label_green = 1;
 color_label_blue = 2;
@@ -23,28 +24,26 @@ processbar = waitbar(0, 'processing output');
 
 
 for iter = 1:1:testdatasize
-    a = predictions(iter, 1) + 1;
-    b = predictions(iter, 2) + 1;
-    x01 = predictions(iter, 3);
-    y01 = predictions(iter, 4);
-    d1 = predictions(iter, 5) + 1;
-    %x02 = predictions(iter, 6);
-    %y02 = predictions(iter, 7);
-    %d2 = predictions(iter, 8);
-    
+
+    a_class = predictions(iter, 1);
+    b_class = predictions(iter, 2);
+    y0 = predictions(iter, 3);
+    d_class = predictions(iter, 4);
+    a = a_class + 10;
+    b = b_class + 10;
+    x0 = a / 2;
+    d = (d_class + 1 ) * 10;    
         
     a_label = labels(iter,1);
     b_label = labels(iter,2);
     phase_label = labels(iter,3);
-    mode_label = labels(iter,4);
+    mode_label = 0;
 
     
     % get aperture field from predicted soure pos
     % and get directivity matrix 
-    [Exa1, Eya1, Hxa1, Hya1] = Aperture_field_from_point_source(x01, y01, d1, a, b, lamda, num);
-    %[Exa2, Eya2, Hxa2, Hya2] = Aperture_field_from_point_source(x02, y02, d2, a, b, lamda, num);
+    [Exa1, Eya1, Hxa1, Hya1] = Aperture_field_from_point_source(x0, y0, d, a, b, lamda, num);
     D_r = Get_Directivity_General(Exa1, Eya1, Hxa1, Hya1, a, b, lamda, num);
-    %D_r = Get_Directivity_General(Exa1 + Exa2, Eya1 + Eya2, Hxa1 + Hxa2, Hya1 + Hya2, a, b, lamda, num);
     
     % test beam directivity matrix
     [Ex_label, Ey_label] = Set_E_field(mode_label, phase_label, a_label, b_label, num);
@@ -71,12 +70,12 @@ for iter = 1:1:testdatasize
     results(iter, 8) = phi_r;
     
     %plot results
-    fig = figure( iter );
-    plot_directivity( D, color_label_blue ); % test beam is blue
-    hold on
-    plot_directivity( D_r, color_label_green ); %reverse beam is green
-    hold off
-    maximize_fig(fig);
+   % fig = figure( iter );
+    %plot_directivity( D, color_label_blue ); % test beam is blue
+    %hold on
+    %plot_directivity( D_r, color_label_green ); %reverse beam is green
+    %hold off
+    %maximize_fig(fig);
     %saveas(fig, sprintf('%d th result ', iter));
     
     waitbar(iter/testdatasize)
